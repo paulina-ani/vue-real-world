a3d60a3cace199b123f2f81b5b2bedb2dbeae151
paulina-ani <aniskiewicz.paulina@gmail.com>
2019-12-23 13:59:09 +0100

    EventCard component added to view EventList

---
+++ src/components/EventCard.vue
@@ -0,0 +1,22 @@
+<template>
+    <div>
+        <h4> {{ title }}</h4>
+    </div>
+</template>
+
+<script>
+    export default {
+        name: "EventCard",
+        data() {
+            return {
+                title: "Park Cleanup"
+            }
+        }
+    }
+</script>
+
+<style scoped>
+    h4 {
+        color: green;
+    }
+</style>
--- src/views/EventList.vue
+++ src/views/EventList.vue
@@ -1,14 +1,18 @@
 <template>
     <div>
         <h1>Event list</h1>
-
+        <EventCard></EventCard>
         <router-link :to="{ name: 'event-show', params: { id: '1'} }">Show event</router-link>
     </div>
 </template>
 
 <script>
+    import EventCard from "../components/EventCard";
     export default {
-        name: "EventList"
+        name: "EventList",
+        components: {
+            EventCard
+        }
     }
 </script>
 
