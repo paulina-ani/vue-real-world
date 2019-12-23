e5cec661d518a97824b406fa7192c6edff35de29
paulina-ani <aniskiewicz.paulina@gmail.com>
2019-12-23 14:17:26 +0100

    event data added to Eventcard.vue; styles for EventCard component added

--- src/components/EventCard.vue
+++ src/components/EventCard.vue
@@ -9,14 +9,39 @@
         name: "EventCard",
         data() {
             return {
-                title: "Park Cleanup"
+                event: {
+                    id: 1,
+                    title: "Beach Cleanup",
+                    date: "Tues Aug 19, 2018",
+                    time: "06.00",
+                    attendees: [
+                        { id: "abc123", name: "Paulina Ani" },
+                        { id: "def456", name: "John Doe"}
+                    ]
+                }
             }
         }
     }
 </script>
 
 <style scoped>
-    h4 {
-        color: green;
+    .event-card {
+        padding: 20px;
+        margin-bottom: 24px;
+        transition: all 0.2s linear;
+        cursor: pointer;
+    }
+    .event-card:hover {
+        transform: scale(1.01);
+        box-shadow: 0 3px 12px 0 rgba(0, 0, 0, 0.2), 0 1px 15px 0 rgba(0, 0, 0, 0.19);
+    }
+    .event-card > .title {
+        margin: 0;
+    }
+
+    .event-link {
+        color: black;
+        text-decoration: none;
+        font-weight: 100;
     }
 </style>
