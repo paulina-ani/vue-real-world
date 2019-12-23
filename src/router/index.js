19572392ada5bb8ec12dae3b5082ab3e937cbf41
paulina-ani <aniskiewicz.paulina@gmail.com>
2019-12-23 11:43:13 +0100

    alias added to about page

--- src/router/index.js
+++ src/router/index.js
@@ -21,12 +21,9 @@ const routes = [
     // this generates a separate chunk (about.[hash].js) for this route
     // which is lazy-loaded when the route is visited.
     component: () =>
-      import(/* webpackChunkName: "about" */ "../views/About.vue")
+      import(/* webpackChunkName: "about" */ "../views/About.vue"),
+    alias: "/about"
   },
-  {
-    path:"about",
-    redirect: {name: "about"}
-  }
 ];
 
 const router = new VueRouter({
