ae03edd4a98cfcaac3eb2c5a8b3753ef6fac2ebf
paulina-ani <aniskiewicz.paulina@gmail.com>
2019-12-30 11:22:44 +0100

    BaseButton component added

---
+++ src/components/BaseButton.vue
@@ -0,0 +1,15 @@
+<template>
+    <div>
+        <button><slot></slot></button>
+    </div>
+</template>
+
+<script>
+    export default {
+        name: "BaseButton"
+    }
+</script>
+
+<style scoped>
+
+</style>
