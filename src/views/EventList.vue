160d4f2f51d51df559b8afacb7b15c7b1806dc07
paulina-ani <aniskiewicz.paulina@gmail.com>
2019-12-30 13:36:04 +0100

    Axios and db.json added

--- src/views/EventList.vue
+++ src/views/EventList.vue
@@ -7,10 +7,21 @@
 
 <script>
     import EventCard from "../components/EventCard";
+    import axios from "axios";
     export default {
         name: "EventList",
         components: {
             EventCard
+        },
+        created() {
+            axios
+                .get('http://localhost:3000/events')
+                .then(response => {
+                    console.log(response.data)
+                })
+                .catch(error => {
+                    console.log("There was an error:" + error.response)
+                })
         }
     }
 </script>
